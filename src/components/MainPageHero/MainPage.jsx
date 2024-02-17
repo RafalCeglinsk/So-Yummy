b94ceaca9d0211a43387850b1d2b641f4e35a646
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-17 08:51:29 +0100

    prettier

--- src/components/MainPageHero/MainPage.jsx
+++ src/components/MainPageHero/MainPage.jsx
@@ -1,29 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import { ChooseYourBreakfast } from './ChooseYourBreakfast/ChooseYourBreakfast';
-import {
-  MainWrapper,
-  HeroWrapper,
-  MainTitle,
-  TextContent,
-} from './MainPage.styled';
+import { ChooseYourBreakfast } from "./ChooseYourBreakfast/ChooseYourBreakfast";
+import { HeroWrapper, MainTitle, TextContent } from "./MainPage.styled";
 
 export function MainPage() {
   return (
-  
-      <HeroWrapper>
-        {' '}
-        <MainTitle>
-          {' '}
-          <span>SO</span>Yummy{' '}
-        </MainTitle>
-        <TextContent>
-          {' '}
-          What to cook?" is not only a recipe app, it is, in fact, your
-          cookbook. You can add your own recipes to save them for the future.{' '}
-        </TextContent>
-     
+    <HeroWrapper>
+      {" "}
+      <MainTitle>
+        {" "}
+        <span>SO</span>Yummy{" "}
+      </MainTitle>
+      <TextContent>
+        {" "}
+        What to cook?" is not only a recipe app, it is, in fact, your cookbook.
+        You can add your own recipes to save them for the future.{" "}
+      </TextContent>
       <ChooseYourBreakfast></ChooseYourBreakfast>
-      </HeroWrapper>
+    </HeroWrapper>
   );
 }
