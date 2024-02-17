4cd23bd72d0f50dfd6537dba530514cd3a5d62a8
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-17 22:50:10 +0100

    relocate Searchbar component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,16 @@
 import { MainPage } from "../components/MainPageHero/MainPage";
-
+import SearchBar from "../components/SearchBar/SearchBar";
 import { MainWrapper } from "../components/MainPageHero/MainPage.styled";
+import { SbMobileStyle } from "../components/MainPageHero/MainPage.styled";
 
 const Main = () => {
   return (
     <MainWrapper>
       {" "}
       <MainPage />
- 
+      <SbMobileStyle>
+      <SearchBar />
+      </SbMobileStyle>
     </MainWrapper>
   );
 };
