af24737e99d702ccb7e21e58caf3d00de149a7c0
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-17 22:52:57 +0100

    style ; add media styles

--- src/components/MainPageHero/ChooseYourBreakfast/ChooseYourBreakfast.styled.jsx
+++ src/components/MainPageHero/ChooseYourBreakfast/ChooseYourBreakfast.styled.jsx
@@ -1,40 +1,36 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
- import BgImg from "../../../images/MainPageHero/ChooseYour1x.png"
-import BgImg2x from '../../../images/MainPageHero/ChooseYour2x.png'
-import BgTb2x from "../../../images/MainPageHero/Choose-Tb-2x.png"
+import BgImg from "../../../images/MainPageHero/ChooseYour1x.png";
+import BgImg2x from "../../../images/MainPageHero/ChooseYour2x.png";
+import BgTb2x from "../../../images/MainPageHero/Choose-Tb-2x.png";
+import BgDSK2x from "../../../images/MainPageHero/Choose-DSK-2x.png"
 
 export const SaladContainer = styled.div`
-
-   position: relative;
-    max-width: 100%;
-    width: 320px;
+  position: relative;
+  max-width: 100%;
+  width: 320px;
   height: 296px;
 
-    position: relative;
-    background-size: cover;
+  position: relative;
+  background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  /* background-image: url(${BgImg}); */
-  background-image: -webkit-image-set(
-    url(${BgImg}) 1x,
-    url(${BgImg2x}) 2x
-  );
+
+  background-image: -webkit-image-set(url(${BgImg}) 1x, url(${BgImg2x}) 2x);
   @media screen and (min-width: 768px) {
-  
-  background-image: -webkit-image-set(
-     
-      url(${BgTb2x}) 2x
-    );
+    background-image: -webkit-image-set(url(${BgTb2x}) 2x);
     width: 378px;
     height: 351px;
     margin-bottom: 0;
-    margin-right: -10px;
-    }
-`
+  }
+  @media screen and (min-width: 1440px) {
+    background-image: -webkit-image-set(url(${BgDSK2x}) 2x);
+    width: 578px;
+  height: 539px;
+}
+`;
 
 export const TextContainer = styled.div`
-
   background-color: var(--color-bg-modal);
   color: var(--color-text-2);
   font-weight: 500;
@@ -57,8 +53,13 @@ export const TextContainer = styled.div`
     width: 260px;
     font-size: 14px;
   }
+  @media screen and (min-width: 1440px) {
+    padding: 16px;
+    top: 336px;
+    left: 335px;
+    width: 298px;
+  }
 `;
-
 
 export const LinkRecipies = styled.a`
   display: flex;
@@ -72,4 +73,3 @@ export const LinkRecipies = styled.a`
     color: var(--color-text-4);
   }
 `;
-
