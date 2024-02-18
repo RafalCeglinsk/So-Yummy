0a72537219972fc38fbea85ac0318a18c1a162d9
Piotr Dubiel <piotrdubiel30@gmail.com>
2024-02-18 12:22:42 +0100

    adding shopping list route

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< Updated upstream
-import {lazy} from "react"
-const ShoppingListPage = lazy(() => import("./pages/ShoppingListPage/ShoppingListPage.jsx"))
-=======
+
 import { HomePage } from "./pages/HomePage/HomePage";
 // import Footer from './components/footer/footer'
 import ShoppingListPage from './pages/ShoppingListPage/ShoppingListPage.jsx'
->>>>>>> Stashed changes
-
 function App() {
   return (
     <Routes>
-      <Route path ="/shopping-list" element={
-      <ShoppingListPage/>} />
-      <Route />
-<<<<<<< Updated upstream
-=======
       <Route index element={<HomePage />} />
       {/* <Route path='/footer' element={<Footer/>} /> */}
       <Route path="/shopping-list" element={<ShoppingListPage/>}/>
->>>>>>> Stashed changes
     </Routes>
   );
 }
