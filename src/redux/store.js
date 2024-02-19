d72d651ce98eb0c2f68b51ce0c788cde096461bd
MichalStanaszek <michal.stanaszek@gmail.com>
2024-02-19 23:10:35 +0100

    Add auth logic to the store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,35 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import shoppingReducer from "./shoppingList/sliceShopping.js";
+import { authReducer } from "./auth/authSlice";
 import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER
-} from "redux-persist"
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
-import shoppingReducer from './shoppingList/sliceShopping.js'
-export const store = configureStore ({
-    reducer: {
-        shopping: shoppingReducer
-    },
-    middleware: (getDefaultMiddleware) => 
+export const store = configureStore({
+  reducer: {
+    shopping: shoppingReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+  },
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions:[FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-        }
-    })
-})
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-
-
+export const persistor = persistStore(store);
