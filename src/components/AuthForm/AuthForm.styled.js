4375b267e53a155a55368c65d8b100f5d0f8acc7
MichalStanaszek <michal.stanaszek@gmail.com>
2024-02-19 20:29:48 +0100

    Create AuthForm.styled.js

---
+++ src/components/AuthForm/AuthForm.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
