c311d8d75a2d9718e760aae35ab39d4a2a15f801
MichalStanaszek <michal.stanaszek@gmail.com>
2024-02-20 23:44:25 +0100

    add register and login routes + hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { refreshUser } from "./redux/auth/operations.js";
+
 import { HomePage } from "./pages/HomePage/HomePage";
 import Favorite from "./pages/FavoritePage/Favorite.jsx";
-
+import ShoppingListPage from "./pages/ShoppingListPage/ShoppingListPage.jsx";
 import Main from "./pages/Main";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import { RestrictedRoute } from "./components/RestrictedRoute/RestrictedRoute.jsx";
 // import Footer from './components/footer/footer'
-import ShoppingListPage from './pages/ShoppingListPage/ShoppingListPage.jsx'
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      
-      <Route path="/shopping-list" element={<ShoppingListPage/>}/>
+      <Route
+        path="/auth/register"
+        element={
+          <RestrictedRoute redirectTo="/main" component={<RegisterPage />} />
+        }
+      />
+      <Route
+        path="/auth/login"
+        element={
+          <RestrictedRoute redirectTo="/main" component={<LoginPage />} />
+        }
+      />
+      <Route path="/shopping-list" element={<ShoppingListPage />} />
       <Route />
       <Route index element={<HomePage />} />
-      <Route path="/main" element={<Main/>}/>
+      <Route path="/main" element={<Main />} />
       <Route path="/favorite" element={<Favorite />} />
       {/* <Route path='/footer' element={<Footer/>} /> */}
     </Routes>
