58f8922aa034ef23da6748d60e18c718099d2494
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-20 14:51:48 +0100

    Fix Import path -  Misclick

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,7 @@ import { MainPage } from "../components/MainPageHero/MainPage";
 import SearchBar from "../components/SearchBar/SearchBar";
 import { MainWrapper } from "../components/MainPageHero/MainPage.styled";
 import { SbMobileStyle } from "../components/MainPageHero/MainPage.styled";
-import PreviewCategories from "../components/MainPageHero/PreviewCategories/PreviewCategories";
+import { PreviewCategories } from "../components/MainPageHero/PreviewCategories/PreviewCategories";
 import { GalleryElement } from "../components/GalleryElement/GalleryElement";
 const Main = () => {
   return (
@@ -10,9 +10,9 @@ const Main = () => {
       {" "}
       <MainPage />
       <SbMobileStyle>
-      <SearchBar />
+        <SearchBar />
       </SbMobileStyle>
- <GalleryElement/>
+      <PreviewCategories />
     </MainWrapper>
   );
 };
