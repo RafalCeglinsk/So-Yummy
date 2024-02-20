7fcbe4870d94f006afb3db8f205816fdf7a4643a
MichalStanaszek <michal.stanaszek@gmail.com>
2024-02-20 22:47:14 +0100

    Update AuthForm.jsx
    - add state and events handlers
    - add react router Link on the bottom

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,11 +1,41 @@
-import { Form } from "./AuthForm.styled";
-
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { login, register } from "../../redux/auth/operations";
+
+import { Form, StyledLink } from "./AuthForm.styled";
 
 export const AuthForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
   const location = useLocation().pathname;
+
+  const handleChangeInput = (event) => {
+    const input = event.target;
+
+    input.name === "name" && setName(input.value);
+    input.name === "email" && setEmail(input.value);
+    input.name === "password" && setPassword(input.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (location === "/auth/register") {
+      if (name === "" || email === "" || password === "") return;
+      dispatch(register({ name, email, password }));
+    }
+
+    if (location === "/auth/login") {
+      if (email === "" || password === "") return;
+      dispatch(login({ email, password }));
+    }
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <h2>{location === "/auth/login" ? "Sign In" : "Registration"}</h2>
       {location === "/auth/register" && (
         <label>
@@ -14,8 +44,8 @@ export const AuthForm = () => {
             label="Name"
             name="name"
             type="text"
-            //  value={name}
-            //  onChange={handleChangeInput}
+            value={name}
+            onChange={handleChangeInput}
             required
           />
         </label>
@@ -26,8 +56,8 @@ export const AuthForm = () => {
           label="Email"
           name="email"
           type="email"
-          //  value={email}
-          //  onChange={handleChangeInput}
+          value={email}
+          onChange={handleChangeInput}
           required
         />
       </label>
@@ -37,8 +67,8 @@ export const AuthForm = () => {
           label="Password"
           name="password"
           type="password"
-          //   value={password}
-          //   onChange={handleChangeInput}
+          value={password}
+          onChange={handleChangeInput}
           minLength={7}
           required
         />
@@ -46,6 +76,11 @@ export const AuthForm = () => {
       <button type="submit">
         {location === "/auth/register" ? "Sign up" : "Sign In"}
       </button>
+      {location === "/auth/login" ? (
+        <StyledLink to="/auth/register">Sign up</StyledLink>
+      ) : (
+        <StyledLink to="/auth/login">Sign in</StyledLink>
+      )}
     </Form>
   );
 };
