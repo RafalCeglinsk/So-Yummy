d2b4733a31bfcc3bc4780ec1f180f30a321c06d2
MichalStanaszek <michal.stanaszek@gmail.com>
2024-02-21 22:10:48 +0100

    add responsivness to Home componenet

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,33 @@
-import { BackgroundSection, Div, H1, P } from "./Home.styled";
+import { NavLink } from "react-router-dom";
+import { StartLogoSmall, StartLogoBig } from "../RenderSvg/RenderSvg";
+import { useWindowWidth } from "../../hooks/useWindowWidth";
 import {
   CurvedButtonColor,
   CurvedButtonTransparent,
 } from "../Buttons/CurvedButton";
-import { StartLogoSmall } from "../RenderSvg/RenderSvg";
-import bgPhoto from "../../images/HomePage/mobileStartx1.png";
-import { NavLink } from "react-router-dom";
+import { BackgroundSection, Div, H1, P } from "./Home.styled";
+import mobileSmallPhoto from "../../images/HomePage/mobileStartx1.jpg";
+import mobileBigPhoto from "../../images/HomePage/mobileStartx2.jpg";
+import tabletSmallPhoto from "../../images/HomePage/tabletStartx1.jpg";
+import tabletBigPhoto from "../../images/HomePage/tabletStartx2.jpg";
+import desktopSmallPhoto from "../../images/HomePage/desktopStartx1.jpg";
+import desktopBigPhoto from "../../images/HomePage/desktopStartx2.jpg";
 
 export const Home = () => {
+  const width = useWindowWidth();
+  const isDesktop = width >= 768;
+
   return (
-    <BackgroundSection image={bgPhoto}>
+    <BackgroundSection
+      mobileimage={mobileSmallPhoto}
+      mobileiretinaimage={mobileBigPhoto}
+      tabletimage={tabletSmallPhoto}
+      tabletretinaimage={tabletBigPhoto}
+      desktopimage={desktopSmallPhoto}
+      desktopretinaimage={desktopBigPhoto}
+    >
       <Div>
-        <StartLogoSmall />
+        {isDesktop ? <StartLogoBig /> : <StartLogoSmall />}
         <H1>Welcome to the app!</H1>
         <P>
           This app offers more than just a collection of recipes - it is
--- src/components/Home/Home.styled.jsx
+++ src/components/Home/Home.styled.jsx
@@ -12,19 +12,47 @@ export const Div = styled.div`
 export const BackgroundSection = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url(${(props) => props.image});
+  background-image: url(${(props) => props.mobileiretinaimage});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+
+  @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+    background-image: url(${(props) => props.mobileiretinaimage});
+  }
+
+  @media (min-width: 768px) {
+    background-image: url(${(props) => props.tabletimage});
+
+    @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+      background-image: url(${(props) => props.tabletretinaimage});
+    }
+  }
+
+  @media (min-width: 1440px) {
+    background-image: url(${(props) => props.desktopimage});
+
+    @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+      background-image: url(${(props) => props.desktopretinaimage});
+    }
+  }
 `;
 
 export const H1 = styled.h1`
   padding: 24px 0 14px;
   font-size: 24px;
+
+  @media (min-width: 768px) {
+    font-size: 28px;
+  }
 `;
 
 export const P = styled.p`
   margin: 0px 35px;
   text-align: center;
   padding-bottom: 44px;
+
+  @media (min-width: 768px) {
+    font-size: 18px;
+  }
 `;
