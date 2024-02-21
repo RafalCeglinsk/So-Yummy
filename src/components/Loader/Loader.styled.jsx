387be742fed17fa50405cebcaf455d071bf42e73
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-21 09:38:07 +0100

    feat loader create and style

---
+++ src/components/Loader/Loader.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LoaderStyle = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+
+  z-index: 5;
+  background-color: rgba(255, 255, 255, 0.1);
+  padding: 20px;
+  border-radius: 10px;
+`;
