0fde8776e19d8d8c26b962922403379cc3f8a60c
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-22 12:35:07 +0100

    replace button component

---
+++ src/components/Buttons/GalleryButtonSeeAll.jsx
@@ -0,0 +1,8 @@
+import { StyleSeeAll } from "./GalleryButtonSeeAll.styled"
+
+export const ButtonSeeAll = (props)=>{
+
+    return(
+      <StyleSeeAll>{props.text}</StyleSeeAll> 
+    )
+}
--- src/components/GalleryElement/GalleryButtonSeeAll.jsx
+++
@@ -1,8 +0,0 @@
-import { StyleSeeAll } from "./GalleryButtonSeeAll.styled"
-
-export const ButtonSeeAll = ()=>{
-
-    return(
-      <StyleSeeAll>See all</StyleSeeAll> 
-    )
-}
