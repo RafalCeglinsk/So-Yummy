58bbdb7b3558d2443d9bc070cddf2dfaed6283c0
Piotr Dubiel <piotrdubiel30@gmail.com>
2024-02-22 19:58:54 +0100

    update

--- src/redux/shoppingList/thunkShopping.js
+++ src/redux/shoppingList/thunkShopping.js
@@ -1,37 +1,58 @@
-import {createAsyncThunk} from "@reduxjs/toolkit"
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-import axios from "axios"
+
+
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/api', 
+    baseURL: 'http://localhost:5001/api',
     timeout: 5000,
-    headers: {'X-Custom-Header': 'foobar'}
+    headers: {
+      'X-Custom-Header': 'foobar'
+    }
   });
+  
+ 
+  instance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('auth');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
 
 export const deleteShoppingThunk = createAsyncThunk("shopping/delete", async (id) => {
-    try {
-        await instance.delete(`/shopping-lists/${id}`);
-        return id;
-    } catch(error) {
-        console.log(error.message);
-        throw error;
-    }
+  try {
+    await instance.delete(`/shopping-lists/${id}`);
+    return id;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
 });
 
 export const getShoppingThunk = createAsyncThunk("shopping/get", async () => {
-    try {
-        const {data} = await instance.get("/shopping-lists")
-        return data.data
-    } catch(error) {
-        console.log(error.message)
-    }
-})
+  try {
+    const { data } = await instance.get("/shopping-lists");
+    return data.data;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+});
 
-export const addShoppingThunk = createAsyncThunk("contacts/post", async(credentials) =>{
-    try {
-        const {data} = await instance.post("/shopping-lists", credentials)
-        return data.data 
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+export const addShoppingThunk = createAsyncThunk("contacts/post", async (credentials) => {
+  try {
+    const { data } = await instance.post("/shopping-lists", credentials);
+    return data.data;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+});
