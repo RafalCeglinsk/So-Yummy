d562110410c12c4ef9020b0ca711d173401f84a1
MichalStanaszek <michal.stanaszek@gmail.com>
2024-02-23 23:48:18 +0100

    add tablet and desktop styles

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -8,19 +8,21 @@ import mobileLogoRetina from "../../images/AuthPage/mobileLogox2.png";
 import mobileBackground from "../../images/AuthPage/mobileBackgroundx1.png";
 import mobileBackgroundRetina from "../../images/AuthPage/mobileBackgroundx2.png";
 
-import tabletLogo from "../../images/AuthPage/tabletBackgroundx1.png";
-import tabletLogoRetina from "../../images/AuthPage/tabletBackgroundx2.png";
+import tabletLogo from "../../images/AuthPage/tabletLogox1.png";
+import tabletLogoRetina from "../../images/AuthPage/tabletLogox2.png";
 import tabletBackground from "../../images/AuthPage/tabletBackgroundx1.png";
 import tabletBackgroundRetina from "../../images/AuthPage/tabletBackgroundx2.png";
 
-import desktopLogo from "../../images/AuthPage/desktopBackgroundx1.png";
-import desktopLogoRetina from "../../images/AuthPage/desktopBackgroundx2.png";
+import desktopLogo from "../../images/AuthPage/desktopLogox1.png";
+import desktopLogoRetina from "../../images/AuthPage/desktopLogox2.png";
 import desktopBackground from "../../images/AuthPage/desktopBackgroundx1.png";
 import desktopBackgroundRetina from "../../images/AuthPage/desktopBackgroundx2.png";
 
 import {
   Background,
   Container,
+  Desktop,
+  Flex,
   Form,
   FormButton,
   H2,
@@ -61,69 +63,73 @@ export const AuthForm = () => {
   };
   return (
     <Form onSubmit={handleSubmit}>
-      <Logo
-        mobileimage={mobileLogo}
-        mobileiretinaimage={mobileLogoRetina}
-        tabletimage={tabletLogo}
-        tabletretinaimage={tabletLogoRetina}
-        desktopimage={desktopLogo}
-        desktopretinaimage={desktopLogoRetina}
-      />
-      <Container>
-        <H2>{location === "/auth/login" ? "Sign In" : "Registration"}</H2>
-        {location === "/auth/register" && (
-          <StyledLabel>
-            <StyledInput
-              label="Name"
-              name="name"
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={handleChangeInput}
-              required
-            />
-          </StyledLabel>
-        )}
-        <StyledLabel>
-          <StyledInput
-            label="Email"
-            name="email"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={handleChangeInput}
-            required
-          />
-        </StyledLabel>
-        <StyledLabel>
-          <StyledInput
-            label="Password"
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handleChangeInput}
-            minLength={7}
-            required
-          />
-        </StyledLabel>
-        <FormButton type="submit">
-          {location === "/auth/register" ? "Sign up" : "Sign In"}
-        </FormButton>
-      </Container>
-      {location === "/auth/login" ? (
-        <StyledLink to="/auth/register">Sign up</StyledLink>
-      ) : (
-        <StyledLink to="/auth/login">Sign in</StyledLink>
-      )}
-      <Background
-        mobilebackground={mobileBackground}
-        mobileiretinabackground={mobileBackgroundRetina}
-        tabletbackground={tabletBackground}
-        tabletretinabackground={tabletBackgroundRetina}
-        desktopbackground={desktopBackground}
-        desktopretinabackground={desktopBackgroundRetina}
-      />
+      <Flex>
+        <Logo
+          mobileimage={mobileLogo}
+          mobileiretinaimage={mobileLogoRetina}
+          tabletimage={tabletLogo}
+          tabletretinaimage={tabletLogoRetina}
+          desktopimage={desktopLogo}
+          desktopretinaimage={desktopLogoRetina}
+        />
+        <Desktop>
+          <Container>
+            <H2>{location === "/auth/login" ? "Sign In" : "Registration"}</H2>
+            {location === "/auth/register" && (
+              <StyledLabel>
+                <StyledInput
+                  label="Name"
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={handleChangeInput}
+                  required
+                />
+              </StyledLabel>
+            )}
+            <StyledLabel>
+              <StyledInput
+                label="Email"
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={handleChangeInput}
+                required
+              />
+            </StyledLabel>
+            <StyledLabel>
+              <StyledInput
+                label="Password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChangeInput}
+                minLength={7}
+                required
+              />
+            </StyledLabel>
+            <FormButton type="submit">
+              {location === "/auth/register" ? "Sign up" : "Sign In"}
+            </FormButton>
+          </Container>
+          {location === "/auth/login" ? (
+            <StyledLink to="/auth/register">Sign up</StyledLink>
+          ) : (
+            <StyledLink to="/auth/login">Sign in</StyledLink>
+          )}
+        </Desktop>
+        <Background
+          mobilebackground={mobileBackground}
+          mobileiretinabackground={mobileBackgroundRetina}
+          tabletbackground={tabletBackground}
+          tabletretinabackground={tabletBackgroundRetina}
+          desktopbackground={desktopBackground}
+          desktopretinabackground={desktopBackgroundRetina}
+        />
+      </Flex>
     </Form>
   );
 };
--- src/components/AuthForm/AuthForm.styled.js
+++ src/components/AuthForm/AuthForm.styled.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
+const device = {
+  tablet: "768px",
+  desktop: "1440px",
+};
+
 export const Form = styled.form`
   position: relative;
   display: flex;
@@ -16,16 +21,52 @@ export const Container = styled.div`
   border-radius: 30px;
   background-color: var(--color-bg-start);
   padding: 32px 28px 40px;
-  z-index: 2;
+  z-index: 20;
+
+  @media (min-width: ${device.tablet}) {
+    max-width: 500px;
+    padding: 44px 50px;
+  }
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  @media (min-width: ${device.desktop}) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 100vw;
+  }
+`;
+
+export const Desktop = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const H2 = styled.h2`
   color: var(--color-default-1);
   padding-bottom: 18px;
+
+  @media (min-width: ${device.tablet}) {
+    padding-bottom: 32px;
+  }
 `;
 
 export const StyledLabel = styled.label`
   padding-bottom: 12px;
+
+  @media (min-width: ${device.tablet}) {
+    padding-bottom: 24px;
+  }
 `;
 
 export const StyledInput = styled.input`
@@ -40,6 +81,12 @@ export const StyledInput = styled.input`
     color: var(--color-default-1);
     opacity: 0.8;
   }
+
+  @media (min-width: ${device.tablet}) {
+    padding: 17.5px;
+    max-width: 400px;
+    font-size: 18px;
+  }
 `;
 
 export const StyledSpan = styled.span`
@@ -53,33 +100,64 @@ export const FormButton = styled.button`
   background-color: var(--color-text-5);
   color: var(--color-default-1);
   border-radius: 6px;
+
+  @media (min-width: ${device.tablet}) {
+    padding: 19px;
+  }
 `;
 
 export const Logo = styled.div`
-  display: block;
   width: 285px;
   height: 250px;
   background-image: url(${(props) => props.mobileimage});
-  background-color: white;
   background-repeat: no-repeat;
   margin-bottom: -32px;
+
+  @media (min-width: ${device.tablet}) {
+    width: 409px;
+    height: 359px;
+    margin-bottom: -15px;
+    background-image: url(${(props) => props.tabletimage});
+  }
+
+  @media (min-width: ${device.desktop}) {
+    width: 532px;
+    height: 468px;
+    z-index: 2;
+    background-image: url(${(props) => props.desktopimage});
+  }
 `;
 
 export const StyledLink = styled(NavLink)`
   color: var(--color-default-1);
+  text-align: center;
   margin: 18px;
   text-decoration: underline;
-  z-index: 1;
+
+  @media (min-width: ${device.tablet}) {
+    font-size: 16px;
+  }
 `;
 
 export const Background = styled.div`
   position: absolute;
-  bottom: 0px;
-  left: 0px;
+  bottom: 0;
+  left: 0;
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
   width: 100vw;
-  height: 55vh;
+  height: 490px;
+  z-index: -50;
   background-image: url(${(props) => props.mobilebackground});
+
+  @media (min-width: ${device.tablet}) {
+    height: 606px;
+    background-image: url(${(props) => props.tabletbackground});
+  }
+
+  @media (min-width: ${device.desktop}) {
+    height: 325px;
+    background-image: url(${(props) => props.desktopbackground});
+  }
 `;
