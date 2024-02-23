117e64cc5b2e96b91f8d3e91c3a92dc58667e8e2
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-02-23 11:54:19 +0100

    Create 404 and render  in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import { useDispatch } from "react-redux";
 import { refreshUser } from "./redux/auth/operations.js";
 
 import { HomePage } from "./pages/HomePage/HomePage";
-// import Footer from './components/footer/footer'
-import Favorite from "./pages/FavoritePage/Favorite.jsx";
-import ShoppingListPage from "./pages/ShoppingListPage/ShoppingListPage.jsx";
-import Main from "./pages/Main";
-import RegisterPage from "./pages/RegisterPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import { RestrictedRoute } from "./components/RestrictedRoute/RestrictedRoute.jsx";
-import { HeaderPage } from "./pages/Header/Header.jsx";
+
+import { HomePage } from "./pages/HomePage/HomePage";
+import { RegisterPage } from "./pages/RegisterPage.jsx";
+import { LoginPage } from "./pages/LoginPage.jsx";
+import { Main } from "./pages/Main";
 import { AddRecipe } from "./pages/AddRecipe/AddRecipe.jsx";
+import Favorite from "./pages/FavoritePage/Favorite.jsx";
+import MyRecipesPage from "./pages/MyRecipies/MyRecipesPage.jsx";
+import ShoppingListPage from "./pages/ShoppingListPage/ShoppingListPage.jsx";
+import { NotFound } from "./pages/NotFound.jsx";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,27 +24,28 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HeaderPage />}>
-        <Route index element={<HomePage />} />
-        <Route
-          path="/auth/register"
-          element={
-            <RestrictedRoute redirectTo="/main" component={<RegisterPage />} />
-          }
-        />
-        <Route
-          path="/auth/login"
-          element={
-            <RestrictedRoute redirectTo="/main" component={<LoginPage />} />
-          }
-        />
-        <Route path="shopping-list" element={<ShoppingListPage />} />
-        <Route />
-        <Route path="/main" element={<Main />} />
-        <Route path="favorite" element={<Favorite />} />
-        <Route path="recipe" element={<AddRecipe />} />
-        {/* <Route path='/footer' element={<Footer/>} /> */}
-      </Route>
+      <Route index element={<HomePage />} />
+      <Route
+        path="/auth/register"
+        element={
+          <RestrictedRoute redirectTo="/main" component={<RegisterPage />} />
+        }
+      />
+      <Route
+        path="/auth/login"
+        element={
+          <RestrictedRoute redirectTo="/main" component={<LoginPage />} />
+        }
+      />
+      <Route path="/main" element={<Main />} />
+      {/* <Route>CATEGORIES</Route> */}
+      <Route path="/add" element={<AddRecipe></AddRecipe>} />
+      <Route path="/favorite" element={<Favorite />} />
+      {/* <Route>RECIPE</Route> */}
+      <Route path="/my-recipie" element={<MyRecipesPage />} />
+      {/* <Route>SEARCH</Route> */}
+      <Route path="/shopping-list" element={<ShoppingListPage />} />
+      <Route path="*" element={<NotFound />}/>
     </Routes>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+export const  NotFound = ()=> {
+    return(
+<>
+<p>ERROR 404 CAN NOT FIND</p>
+</>
+
+    )
+}
+
