6da7eb29bf366bcab60833912395aa3ea68d4c14
MikolajBajdalski <bajdalm033@gmail.com>
2024-02-26 17:07:10 +0100

    Update RecipeDescriptionFields.jsx

--- src/components/AddRecipe/RecipeDescriptionFields/RecipeDescriptionFields.jsx
+++ src/components/AddRecipe/RecipeDescriptionFields/RecipeDescriptionFields.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import Notiflix from "notiflix";
 import { useDispatch, useSelector } from "react-redux";
 import Select from "react-select";
 import {
@@ -15,6 +16,7 @@ import {
   ImageUploadContainer,
   ImageUploadButton,
   Form,
+  ImagePreview,
 } from "./styles";
 
 const timeOptions = [
@@ -34,17 +36,33 @@ const timeOptions = [
   { value: "70", label: "70 min" },
   { value: "75", label: "75 min" },
   { value: "80", label: "80 min" },
+  { value: "85", label: "85 min" },
+  { value: "90", label: "90 min" },
+  { value: "95", label: "95 min" },
+  { value: "100", label: "100 min" },
+  { value: "105", label: "105 min" },
+  { value: "110", label: "110 min" },
+  { value: "115", label: "115 min" },
+  { value: "120", label: "120 min" },
 ];
 
 const ImageUploadField = ({ onImageUpload, setRecipeData }) => {
   const fileInputRef = useRef(null);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("");
 
   const handleImageChange = (e) => {
     if (e.target.files[0]) {
-      setRecipeData((prevRecipeData) => ({
-        ...prevRecipeData,
-        recipeImg: e.target.files[0],
-      }));
+      const fileReader = new FileReader();
+
+      fileReader.onloadend = () => {
+        setImagePreviewUrl(fileReader.result);
+        setRecipeData((prevRecipeData) => ({
+          ...prevRecipeData,
+          recipeImg: e.target.files[0],
+        }));
+      };
+
+      fileReader.readAsDataURL(e.target.files[0]);
     }
   };
 
@@ -52,13 +70,27 @@ const ImageUploadField = ({ onImageUpload, setRecipeData }) => {
     fileInputRef.current.click();
   };
 
+  useEffect(() => {
+    return () => {
+      // Oczyszczenie URL obrazka, aby uniknąć wycieków pamięci
+      imagePreviewUrl && URL.revokeObjectURL(imagePreviewUrl);
+    };
+  }, [imagePreviewUrl]);
+
   return (
-    <ImageUploadContainer onClick={handleContainerClick}>
-      <label htmlFor="recipeImg" style={{ cursor: "pointer" }}>
-        <ImageUploadButton type="button">
-          <CameraIcon />
-        </ImageUploadButton>
-      </label>
+    <ImageUploadContainer
+      onClick={handleContainerClick}
+      isImageUploaded={!!imagePreviewUrl}
+    >
+      {imagePreviewUrl ? (
+        <ImagePreview src={imagePreviewUrl} alt="Recipe" />
+      ) : (
+        <label htmlFor="recipeImg" style={{ cursor: "pointer" }}>
+          <ImageUploadButton type="button">
+            <CameraIcon />
+          </ImageUploadButton>
+        </label>
+      )}
       <input
         ref={fileInputRef}
         id="recipeImg"
@@ -82,6 +114,7 @@ const RecipeDescriptionFields = ({ recipeData, setRecipeData }) => {
 
   const handleImageUpload = (file) => {
     console.log(file);
+
     dispatch(updateField({ name: "recipeImg", value: file }));
   };
 
@@ -95,10 +128,20 @@ const RecipeDescriptionFields = ({ recipeData, setRecipeData }) => {
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
+    if (!recipeData.title.trim()) {
+      Notiflix.Notify.failure("Title is required.");
+      return;
+    }
+
+    if (!recipeData.description.trim()) {
+      Notiflix.Notify.failure("Description is required.");
+      return;
+    }
+
     const formData = new FormData();
     formData.append("title", recipeData.title);
     formData.append("description", recipeData.description);
-    // Dodaj plik obrazu
+
     const imageInput = document.getElementById("image-upload");
     if (imageInput.files[0]) {
       formData.append("recipeImg", imageInput.files[0]);
@@ -109,11 +152,16 @@ const RecipeDescriptionFields = ({ recipeData, setRecipeData }) => {
         method: "POST",
         body: formData,
       });
+
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
+
+      // Sukces
+      Notiflix.Notify.success("Recipe added successfully.");
     } catch (error) {
       console.error("Upload error:", error);
+      Notiflix.Notify.failure("Failed to add the recipe.");
     }
   };
 
