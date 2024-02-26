51fba4296811e1c582a9eb4baa006644168a4b17
Stefan Bielak <117030658+StefanBielak@users.noreply.github.com>
2024-02-26 12:52:08 +0100

    error_404-added

---
+++ src/components/404/error_404.styled.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 138px;
+`;
+
+export const ErrorImage = styled.img`
+  width: 498px;
+  height: 331px;
+`;
+
+export const ErrorText = styled.p`
+  font-size: 18px;
+  margin-top: 14px;
+  font-weight: 400;
+  color: #000000;
+  color: rgba(51, 51, 51, 0.5);
+
+`;
+export const ErrorHeading = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  color: #000000;
+  margin-bottom: 20px;
+  margin-top: 32px;
+`;
