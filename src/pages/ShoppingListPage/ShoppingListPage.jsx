b711e6d8f6b2a5ac94e0162f1348617945162f12
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-02-26 12:13:27 +0100

    Update ShoppingListPage.jsx

--- src/pages/ShoppingListPage/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage/ShoppingListPage.jsx
@@ -1,11 +1,6 @@
 import IngredientsShoppingList from "../../components/IndegredientsShoppingList/IndegredientsShoppingList";
 import { ShoppingPageStyled } from "./ShoppingListPageStyled.js";
 import { MainTitle } from "../../components/MainPageTittle/MainPageTittle.jsx";
-import IngredientsShoppingList from "../../components/IndegredientsShoppingList/IndegredientsShoppingList";
-import {
-  ShoppingPageStyled,
-  TitleShoppingPage,
-} from "./ShoppingListPageStyled.js";
 
 const ShoppingListPage = () => {
   return (
