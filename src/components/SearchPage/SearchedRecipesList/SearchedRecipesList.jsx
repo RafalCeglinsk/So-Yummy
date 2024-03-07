5d27550df485c2e44112f772784a8ab150f02dd9
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-03-07 11:56:46 +0100

    prettier

--- src/components/SearchPage/SearchedRecipesList/SearchedRecipesList.jsx
+++ src/components/SearchPage/SearchedRecipesList/SearchedRecipesList.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
-import { GalleryUl,GalleryLi, RecipeImg,RecipeDescription} from "./SearchedRecipesList.styled";
-
+import {
+  GalleryUl,
+  GalleryLi,
+  RecipeImg,
+  RecipeDescription,
+} from "./SearchedRecipesList.styled";
 
 export const SearchedRecipesList = ({ recipes }) => {
   return (
     <GalleryUl>
       {recipes.map((recipe, _id) => (
-          <Link to={`/recipes/${recipe._id}`} key={_id}>
-        <GalleryLi key={recipe._id}>
-          <RecipeImg src={recipe.thumb}></RecipeImg>
-   
-       <RecipeDescription>       <p>{recipe.title}</p></RecipeDescription>
-        </GalleryLi>
+        <Link to={`/recipes/${recipe._id}`} key={_id}>
+          <GalleryLi key={recipe._id}>
+            <RecipeImg src={recipe.thumb}></RecipeImg>
+            <RecipeDescription>
+              <p>{recipe.title}</p>
+            </RecipeDescription>
+          </GalleryLi>
         </Link>
       ))}
     </GalleryUl>
