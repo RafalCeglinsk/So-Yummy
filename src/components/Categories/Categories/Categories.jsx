ed62f0da1924faf5539b6f4e265b936493db9d94
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-03-07 11:03:27 +0100

    replace "h2" to component mainTitle

--- src/components/Categories/Categories/Categories.jsx
+++ src/components/Categories/Categories/Categories.jsx
@@ -1,11 +1,10 @@
-import { H2 } from "./Categories.styled";
+
 import { HorizontalScrollList } from "../HorizontalScrollList/HorizontalScrollList";
 import { GalleryElement } from "../../GalleryElement/GalleryElement";
 
 export const Categories = () => {
   return (
     <div>
-      <H2>Categories</H2>
       <HorizontalScrollList />
       <GalleryElement />
     </div>
